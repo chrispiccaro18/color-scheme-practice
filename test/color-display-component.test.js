5bca8001bfe9b7f48ba8f12f4914bdd01fa8233a
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-16 10:07:03 -0700

    test passing dynamically

--- test/color-display-component.test.js
+++ test/color-display-component.test.js
@@ -2,11 +2,11 @@ const test = QUnit.test;
 
 QUnit.module('Create color display');
 
-function createColorDisplay() {
+function createColorDisplay(color) {
     const html = /*html*/`
         <section class="color-display">
-            <p>#665162</p>
-            <div style="background-color: #665162;"></div>
+            <p>#${color}</p>
+            <div style="background-color: #${color};"></div>
         </section>
     `;
     const template = document.createElement('template');
@@ -22,8 +22,9 @@ test('make color display dom', assert => {
             <div style="background-color: #665162;"></div>
         </section>
     `;
+    const color = '665162';
     //act
-    const result = createColorDisplay();
+    const result = createColorDisplay(color);
     //assert
     assert.htmlEqual(result, expected);
 });
