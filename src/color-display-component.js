54f74d3ef244b670545f8509bcaa64e52e0bdd5b
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-16 10:08:21 -0700

    moved create color display to its own module

---
+++ src/color-display-component.js
@@ -0,0 +1,11 @@
+export function createColorDisplay(color) {
+    const html = /*html*/`
+        <section class="color-display">
+            <p>#${color}</p>
+            <div style="background-color: #${color};"></div>
+        </section>
+    `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
--- test/color-display-component.test.js
+++ test/color-display-component.test.js
@@ -1,18 +1,8 @@
+import { createColorDisplay } from '../src/color-display-component.js';
+
 const test = QUnit.test;
 
 QUnit.module('Create color display');
-
-function createColorDisplay(color) {
-    const html = /*html*/`
-        <section class="color-display">
-            <p>#${color}</p>
-            <div style="background-color: #${color};"></div>
-        </section>
-    `;
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
 
 test('make color display dom', assert => {
     //arrange
