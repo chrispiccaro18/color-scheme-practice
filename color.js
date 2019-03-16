70ff07aa369049cf30a9dc79c01e72dff5f40310
Chris Piccaro <chrispiccaro18@gmail.com>
2019-03-16 11:39:18 -0700

    Added original color to begining of array

--- color.js
+++ color.js
@@ -1,17 +1,23 @@
 import loadColorDisplay from './src/color-display-component.js';
 import { createSchemeUrl } from './src/create-scheme-url.js';
+import { createColorArray } from './src/create-color-array.js';
 
 // total amount of colors 16777216
 const randomColorForm = document.getElementById('random-color');
 
 randomColorForm.addEventListener('submit', event => {
     event.preventDefault();
-    const colors = [];
-    // for(let i = 0; i < 1; i++) {
-    //     colors.push((16777216 + (Math.random()) * 16777215).toString(16).substr(1, 6));
-    // }
+
     const color = (16777216 + (Math.random()) * 16777215).toString(16).substr(1, 6);
+    console.log(color);
     const url = createSchemeUrl(color);
-    console.log(url);
-    // loadColorDisplay(colors);
+    const originalColor = [color];
+    loadColorDisplay(originalColor);
+    fetch(url)
+        .then(response => response.json())
+        .then(body => {
+            const colors = createColorArray(body);
+            colors.unshift(color);
+            loadColorDisplay(colors);
+        });
 });
